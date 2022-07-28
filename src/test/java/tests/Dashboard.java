1ad37e3acc9e2822636ee54f313e3a1236d208a5
nimmalanaresh <nimmalanaresh48@gmail.com>
2022-07-28 12:00:02 +0530

    kvk

--- src/test/java/tests/Dashboard.java
+++ src/test/java/tests/Dashboard.java
@@ -2,17 +2,21 @@ package tests;
 
 import org.testng.annotations.Test;
 
-import io.appium.java_client.AppiumBy;
-
 public class Dashboard extends Login {
    @Test(priority = 2)
 	public void Dashscroll() {
 		
-	//	 WebElement ele = driver.findElement(By.xpath("//*[@text='Average Basket']"));
-		
-		driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).ScrollIntoView(text(\"Average Basket\"));"));
-
-		 
+//    	 WebElement ele = driver.findElement(By.xpath("//*[@text='Average Basket']"));
+//		
+//		driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).ScrollIntoView(text(\"Average Basket\"));"));
+//
+//		 
+	   
+//	   String uiSelector = "new UiSelector().textMatches(\"" +"Average Basket"+ "\")";
+//
+//String command = "new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView("+ uiSelector + ");";
+//
+//driver.findElementByAndroidUIAutomator(command);
 	}
 	
 }
